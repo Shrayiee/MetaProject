051c55056a704e2af60037e3ba19b01189b48bd4
Shrayiee <122947193+Shrayiee@users.noreply.github.com>
2026-08-09 00:54:08 +0530

    __init__.py

--- __init__.py
+++ __init__.py
@@ -1,25 +1,25 @@
 from fastapi import APIRouter
 from .flow1 import flow1_router
-# from .asit_demo_flow import asit_router
-# from .asit_demo_flow2 import asit_router2
-# from .asit_demo_flow3 import asit_router3
-# from .asit_demo_flow_4 import asit_router4
-# from .asit_demo_flow_5 import asit_router5
-# from .asit_demo_flow_6 import asit_router6
-# from .asit_dynamic_flow import asit_db_dynamic_router
+# from .shray_demo_flow import asit_router
+# from .shray_demo_flow2 import asit_router2
+# from .shray_demo_flow3 import asit_router3
+# from .shray_demo_flow_4 import asit_router4
+# from .shray_demo_flow_5 import asit_router5
+# from .shray_demo_flow_6 import asit_router6
+# from .shray_dynamic_flow import asit_db_dynamic_router
 # from .database_operation_flow import crud_with_db
 # from .demo_practice import demo_router
 from .sign_in import shray_router
 
 flow_router = APIRouter()
 # flow_router.include_router(flow1_router,prefix="/flow1")
-# flow_router.include_router(asit_router,prefix="/asit")
-# flow_router.include_router(asit_router2,prefix="/asit2")
-# flow_router.include_router(asit_router3,prefix="/asit3")
-# flow_router.include_router(asit_router4,prefix="/asit4")
-# flow_router.include_router(asit_router5,prefix="/asit5")
-# flow_router.include_router(asit_router6,prefix="/asit6")
-# flow_router.include_router(asit_db_dynamic_router,prefix="/asit7")
+# flow_router.include_router(shray_router,prefix="/asit")
+# flow_router.include_router(shray_router2,prefix="/asit2")
+# flow_router.include_router(shray_router3,prefix="/asit3")
+# flow_router.include_router(shray_router4,prefix="/asit4")
+# flow_router.include_router(shray_router5,prefix="/asit5")
+# flow_router.include_router(shray_router6,prefix="/asit6")
+# flow_router.include_router(shray_db_dynamic_router,prefix="/asit7")
 # flow_router.include_router(crud_with_db,prefix="/asit8")
 # flow_router.include_router(demo_router,prefix="/asit9")
 flow_router.include_router(shray_router,prefix="/shray")
